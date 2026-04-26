ffb7c143d3dbe0d9c8e7451a667eeb9656284517
NickMcKenna05 <nickmckenna05@gmail.com>
2026-04-26 16:00:58 -0500

    Fix CORS to use ALLOWED_ORIGINS from settings

--- server/main.py
+++ server/main.py
@@ -23,7 +23,7 @@ Base.metadata.create_all(bind=engine)
 # CORS middleware - allows frontend to make requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS if hasattr(settings, "BACKEND_CORS_ORIGINS") else ["http://localhost:3000"],
+    allow_origins=settings.cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
